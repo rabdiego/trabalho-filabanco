d03155247fce066f8904e85db7252d76473ffb6e
Diego <62783181+nikrs14@users.noreply.github.com>
2022-11-22 13:47:48 -0300

    Ajustes no registro feitos

--- logtree.h
+++ logtree.h
@@ -12,7 +12,7 @@ typedef struct _log {
 
 void log_inicializar(Log **);
 void log_registrar(Log **, int, int, int, int);
-float log_media_por_classe(Log *, int);
+float log_media_por_classe(Log **, int);
 int log_obter_soma_por_classe(Log **, int);
 int log_obter_contagem_por_classe(Log **, int);
 
