457c0973f7d05b549fa4e97994a166295d601c23
Nikrs14 <diegorabelodesa@gmail.com>
2022-11-22 19:12:46 -0300

    Formatação inicial da main

--- main.c
+++ main.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "./logtree.h"
 
-int main() {
-    Log *raiz;
-    log_inicializar(&raiz);
-    log_registrar(&raiz, 10, 10, 10, 10);
-    log_registrar(&raiz, 10, 10, 30, 10);
-    log_registrar(&raiz, 10, 10, 30, 10);
-    log_registrar(&raiz, 10, 20, 40, 10);
-    log_registrar(&raiz, 10, 20, 40, 10);
-    printf("%f\n", log_media_por_classe(&raiz, 10));
-    return 1;
+int main(int narg, char * argv[]) {
+    return EXIT_SUCCESS;
 }
