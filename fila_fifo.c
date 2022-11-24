a094407b54dbd820e72aa5904dafbdb1d6f092ca
ArtroxGabriel <artroxgabriel@gmail.com>
2022-11-24 11:21:21 -0300

    funcoes f_obter e as duas f_consultar

--- fila_fifo.c
+++ fila_fifo.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "fila_fifo.h"
 
@@ -13,7 +14,7 @@ void f_inicializar(Fila_FIFO **F){
     (*F) = NULL;
 }
 int f_inserir(Fila_FIFO **F, int chave, int valor){
-    /**Descrição: Registra um cliente no registrador
+    /**Descrição: Registra um cliente no fila
      * Autor: Gabriel
      * 
      * Args: [
@@ -22,8 +23,8 @@ int f_inserir(Fila_FIFO **F, int chave, int valor){
      *      valor : int,
      * ]
      * 
-     * Returns: 0 key duplication or insufficient memory
-     *          1 insertion success.
+     * Returns: 0 - key duplication or insufficient memory
+     *          1 - insertion success.
      * 
     */
     Fila_FIFO *aux, *temp = (Fila_FIFO *) malloc(sizeof(Fila_FIFO));
@@ -31,13 +32,98 @@ int f_inserir(Fila_FIFO **F, int chave, int valor){
         return 0;
     temp->chave = chave;
     temp->valor = valor;
-    temp->prox = *F;
+    temp->prox = NULL;
     aux = *F;
-    while( aux != NULL){
-        if( aux->chave == chave)
-            return 0;
+    if(*F == NULL) *F = temp;
+    else{
+        while( aux->prox != NULL){
+            if( aux->chave == chave)
+                return 0;
+            aux = aux->prox;
+        }
+        aux->prox = temp;
+    }
+    return 1;
+}
+int f_obter_proxima_chave(Fila_FIFO **F){
+    /**Descrição: Obtem a proxima chave da fila e a remove após.
+     * Autor: Gabriel
+     * 
+     * Args: [
+     *      F : Fila_FIFO **,
+     * ]
+     * 
+     * Returns: -1 empty queue
+     *          key's number of next element.
+     * 
+    */
+    int key;
+    Fila_FIFO *aux;
+    aux = (*F);
+    
+    if(aux == NULL)
+        return -1;
+    key = aux->chave;
+    (*F) = (*F)->prox;
+    free(aux);
+    return key;  
+}
+int f_consultar_proxima_chave(Fila_FIFO **F){
+    /**Descrição: consulta o valor do nó cabeça
+     * Autor: Gabriel
+     * 
+     * Args: F : Fila_FIFO **
+     * 
+     * Returns: -1 - empty queue
+     *          key's number of next element
+     * 
+    */  
+    Fila_FIFO *aux;
+    aux = (*F);
+    
+    if(aux == NULL)
+        return -1;
+    return aux->chave;  
+}
+int f_consultar_proxima_valor(Fila_FIFO **F){
+    /**Descrição: consulta o valor do nó cabe;a
+     * Autor: Gabriel
+     * 
+     * Args: F : Fila_FIFO **
+     * 
+     * Returns: -1 - empty queue
+     *          value of next element
+     * 
+    */  
+    Fila_FIFO *aux;
+    aux = (*F);
+    
+    if(aux == NULL)
+        return -1;
+    return aux->valor;  
+}
+
+void print_fila(Fila_FIFO *F){
+    Fila_FIFO *aux = F;
+    while (aux != NULL)
+    {
+        printf("chave: %d\nvalor: %d\n", aux->chave, aux->valor);
         aux = aux->prox;
     }
-    *F = temp;
-    return 1;
+}
+
+int main(){
+    Fila_FIFO *fila;
+    f_inicializar(&fila);
+    
+    for (int i = 1; i<3; i++){
+        if(f_inserir(&fila, i, 10+i)){
+        } else {
+            puts("repetido");
+            return EXIT_FAILURE;
+        }
+    }
+    print_fila(fila);
+    printf("\n%d\n\n", f_consultar_proxima_valor(&fila));
+    return EXIT_SUCCESS;
 }
