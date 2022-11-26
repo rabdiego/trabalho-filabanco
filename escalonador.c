cdc780f198004bc760649819212a769e7acbeebf
Tobnobre <tobiasnbb@gmail.com>
2022-11-25 21:31:41 -0300

    vasco

--- escalonador.c
+++ escalonador.c
@@ -1,1 +1,15 @@
-// Teste aaaaa
+#include "escalonador.h"
+#include <stdlib.h>
+
+void e_inicializar (Escalonador *e, int caixas, int delta_t, int n_1, int n_2, int n_3, int n_4, int n_5){
+    n_1 = NULL;
+    n_2 = NULL;
+    n_3 = NULL;
+    n_4 = NULL;
+    n_5 = NULL;
+
+};
+
+int e_inserir_por_fila (Escalonador *e, int classe, int num_conta, int qtde_operacoes){
+
+};
