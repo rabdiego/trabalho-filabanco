916435cfa84118bceed045eab36b866b2732ca54
Nikrs14 <diegorabelodesa@gmail.com>
2022-11-25 21:43:45 -0300

    Branch nova Diego

--- escalonador.c
+++ escalonador.c
@@ -2,12 +2,7 @@
 #include <stdlib.h>
 
 void e_inicializar (Escalonador *e, int caixas, int delta_t, int n_1, int n_2, int n_3, int n_4, int n_5){
-    n_1 = NULL;
-    n_2 = NULL;
-    n_3 = NULL;
-    n_4 = NULL;
-    n_5 = NULL;
-
+    // Minha func
 };
 
 int e_inserir_por_fila (Escalonador *e, int classe, int num_conta, int qtde_operacoes){
