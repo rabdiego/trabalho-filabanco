9b2c143a12b7a304cfd8b50be856d59f4bab8f37
Nikrs14 <diegorabelodesa@gmail.com>
2022-11-26 14:51:35 -0300

    Criação da função e_inserir_por_fila

--- escalonador.c
+++ escalonador.c
@@ -21,7 +21,6 @@ void e_inicializar (Escalonador *e, int caixas, int delta_t, int n_1, int n_2, i
      * 
     */
     
-    
     e->caixas = caixas;
     e->delta_t = delta_t;
     e->n_1 = n_1;
@@ -29,13 +28,46 @@ void e_inicializar (Escalonador *e, int caixas, int delta_t, int n_1, int n_2, i
     e->n_3 = n_3;
     e->n_4 = n_4;
     e->n_5 = n_5;
-    f_inicializar(&e->fila_1);
-    f_inicializar(&e->fila_2);
-    f_inicializar(&e->fila_3);
-    f_inicializar(&e->fila_4);
-    f_inicializar(&e->fila_5);
+    f_inicializar(&(e->fila_1));
+    f_inicializar(&(e->fila_2));
+    f_inicializar(&(e->fila_3));
+    f_inicializar(&(e->fila_4));
+    f_inicializar(&(e->fila_5));
 };
 
 int e_inserir_por_fila(Escalonador *e, int classe, int num_conta, int qtde_operacoes) {
+    /**Descrição: Insere um cliente no escalonador
+     * Autor: Diego
+     * 
+     * Args : [
+     *      e : Escalonador *,
+     *      classe : int,
+     *      num_conta : int,
+     *      qtde_operacoes : int
+     * ]
+     * 
+     * Returns : int
+    */
     
+    switch (classe) {
+        case 5:
+            f_inserir(&(e->fila_5), num_conta, qtde_operacoes);
+            break;
+        case 4:
+            f_inserir(&(e->fila_5), num_conta, qtde_operacoes);
+            break;
+        case 3:
+            f_inserir(&(e->fila_5), num_conta, qtde_operacoes);
+            break;
+        case 2:
+            f_inserir(&(e->fila_5), num_conta, qtde_operacoes);
+            break;
+        case 1:
+            f_inserir(&(e->fila_5), num_conta, qtde_operacoes);
+            break;
+        default:
+            return 0;
+            break;
+    }
+    return 1;
 }
