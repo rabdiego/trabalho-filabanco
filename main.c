45f726baad01ce68eda62a74cbf9ea7d5931a89c
ArtroxGabriel <artroxgabriel@gmail.com>
2022-12-04 16:03:21 -0300

    conserto do '*' no main

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include "./escalonador.h"
 
 int main(int narg, char * argv[]) {
-    Escalonador *e;
-    e_rodar(e, "entrada-0001.txt", "saida-0001.txt");
+    Escalonador e;
+    e_rodar(&e, "entrada-0001.txt", "saida-0001.txt");
     return EXIT_SUCCESS;
 }
